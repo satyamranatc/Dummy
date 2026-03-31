a184fe4bf5db91d4cc8e83b97b1a012320680301
Satyam Rana <satyamranatc@gmail.com>
2026-03-31 20:41:51 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <center>
+        <h1 className='text-8xl bg-indigo-500 p-5 text-white' >Frontend</h1>
+      </center>
+    </div>
+  )
+}
