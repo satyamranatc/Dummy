ddfafe59e2bb0b75155d504b876e7943b78f0fd4
Satyam Rana <satyamranatc@gmail.com>
2026-03-31 21:14:57 +0530

    Sencond Commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ export default function App() {
     <div>
       <center>
         <h1 className='text-8xl bg-indigo-500 p-5 text-white' >Frontend</h1>
+        <p className='m-3 p-4 bg-rose-500 text-white animte-pulse text-lg' >This is the new Line</p>
       </center>
     </div>
   )
