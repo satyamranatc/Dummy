816c35c8f1bbc4ad07d174bf2f0123bfa9799395
Satyam Rana <satyamranatc@gmail.com>
2026-04-02 21:14:36 +0530

    Fourth

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
     <div>
       <center>
         <h1 className='text-8xl bg-indigo-500 p-5 text-white' >Frontend</h1>
-        <p className='m-3 p-4 bg-rose-500 text-white animte-pulse text-lg' >Wecome To Frontend</p>
+        <p className='m-3 p-4 bg-rose-500 text-white animte-pulse text-lg' >Wecome To Frontend V2</p>
       </center>
     </div>
   )
